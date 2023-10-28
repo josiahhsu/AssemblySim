6c8b69ea2d967bf8279bf6e8e90cd2b564998b06
josiahhsu <77859877+josiahhsu@users.noreply.github.com>
2023-10-28 16:00:58 -0500

    add more test cases

--- assemblyTests.js
+++ assemblyTests.js
@@ -25,11 +25,11 @@ function run_tests()
 
     if (failures.length == 0)
     {
-        console.log("All tests passed!")
+        alert("All tests passed!")
     }
     else
     {
-        console.log(`${failures.length} failed tests.`)
+        alert(`${failures.length} failed tests: ${failures}`)
         for (const f of failures)
             console.log(f);
     }
@@ -41,21 +41,39 @@ function get_tests()
     let tests = [
         // instructions
         ["Add immediate", "add $8 %rax", "8"],
-        ["Add register", "add $8 %rsi\nadd %rsi %rax", "8"],
+        ["Add register", "add $8 %rsi\n add %rsi %rax", "8"],
         ["Sub immediate", "sub $8 %rax", "-8"],
-        ["Sub register", "add $8 %rsi\nsub %rsi %rax", "-8"],
-        ["Mul immediate", "add $1 %rax\nmul $8 %rax", "8"],
-        ["Mul register", "add $1 %rax\nadd $8 %rsi\nmul %rsi %rax", "8"],
-        ["Div immediate", "add $10 %rax\ndiv $2 %rax", "5"],
-        ["Div register", "add $10 %rax\nadd $2 %rsi\ndiv %rsi %rax", "5"],
-        ["Integer division", "add $11 %rax\ndiv $2 %rax", "5"],
+        ["Sub register", "add $8 %rsi\n sub %rsi %rax", "-8"],
+        ["Mul immediate", "add $1 %rax\n mul $8 %rax", "8"],
+        ["Mul register", "add $1 %rax\n add $8 %rsi\n mul %rsi %rax", "8"],
+        ["Div immediate", "add $10 %rax\n div $2 %rax", "5"],
+        ["Div register", "add $10 %rax\n add $2 %rsi\n div %rsi %rax", "5"],
+        ["Integer division", "add $11 %rax\n div $2 %rax", "5"],
+        ["Increment", "inc %rax", "1"],
+        ["Decrement", "dec %rax", "-1"],
+        ["Negate", "add $1 %rax\n neg %rax", "-1"],
+        ["Not", "not %rax", "-1"],
+        ["Left arithmetic shift", "add $1 %rax\n sal $1 %rax", "2"],
+        ["Left logical shift", "add $1 %rax\n shl $1 %rax", "2"],
+        ["Right arithmetic shift 1", "add $2 %rax\n sar $1 %rax", "1"],
+        ["Right arithmetic shift 2", "not %rax\n sar $1 %rax", String((~0)>>1)],
+        ["Right logical shift 1", "add $2 %rax\n shr $1 %rax", "1"],
+        ["Right logical shift 2", "not %rax\n shr $1 %rax", String((~0)>>>1)],
+        ["And", "add $7 %rax\n and $1 %rax", "1"],
+        ["Or", "add $6 %rax\n or $1 %rax", "7"],
+        ["Xor", "add $123791 %rax\n xor %rax %rax", "0"],
+        ["Mov", "add $8 %rsi\n mov %rsi %rax", "8"],
+        ["Push-pop", "push $30\n pop %rax", "30", "30"],
 
         // error checking
         ["Bad instruction", "dad $8 %rax", "ERROR"],
         ["No operands", "add", "ERROR"],
         ["Not enough operands", "add $1", "ERROR"],
-        ["Too many operands", "add $1 $1 $1", "ERROR"],
+        ["Too many operands", "add $1 %rax %rax", "ERROR"],
         ["Invalid operands", "add 1 %rax", "ERROR"],
+        ["Invalid immediate", "add $one %rax", "ERROR"],
+        ["Negative immediate", "add $-1 %rax", "ERROR"],
+        ["Invalid register", "add $1 %foo", "ERROR"],
     ]
 
     return tests;
